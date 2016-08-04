8461144cc9fad0cea73a1b238a8f92b7da92666c
drsockmonkee <drsockmonkee@gmail.com>
2016-08-04 08:32:34 -0500

    Added in some of the JSF Navigation with Managed Beans.
    the noresults.xhtml page is added and from the 'results' page you hit
    the 'Submit' button again and get an error page..
    
    Added the 'SearchPlants.java' class as well.

---
+++ PlantPlaces/src/com/plantplaces/ui/SearchPlants.java
@@ -0,0 +1,15 @@
+package com.plantplaces.ui;
+
+import javax.annotation.ManagedBean;
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Scope;
+
+@Named
+@ManagedBean
+@Scope("session")
+public class SearchPlants {
+	public String execute(){
+		return "noresults";
+	}
+}
